1980dd722c57e8a86c5010a23a76c02c18005a95
reagankporter <reagankporter@gmail.com>
2021-11-22 15:53:54 -0500

    db change

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
-// const Sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 
 // // const sequelize = new Sequelize("postgres://postgres:6655a365d362406995af1a41383ebb21@localhost:5432/Travel-Server");
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//     dialect:"postgres",
-//     ssl: process.env.ENVIROMENT === "production"
-// });
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:"postgres",
+    ssl: process.env.ENVIROMENT === "production"
+});
 
-
-// module.exports = sequelize;
-
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize("postgres://postgres:maple2323@localhost:5432/selfq");
 
 module.exports = sequelize;
+
+// const Sequelize = require('sequelize');
+
+// const sequelize = new Sequelize("postgres://postgres:maple2323@localhost:5432/selfq");
+
+// module.exports = sequelize;
