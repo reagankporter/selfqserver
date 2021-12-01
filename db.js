928409ae0e78b6c16c21e9e190a353a4750bf70c
reagankporter <reagankporter@gmail.com>
2021-11-30 20:39:12 -0500

    ssl

--- db.js
+++ db.js
@@ -3,7 +3,10 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect:"postgres",
     dialectOptions: {
-    ssl: true,
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
     }
 });
 
