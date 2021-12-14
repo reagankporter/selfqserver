530fc6acec0cd1f2b556b5c807aebd1d60072634
reagankporter <reagankporter@gmail.com>
2021-12-14 17:10:04 -0500

    CHANGE OF SPELLING

--- db.js
+++ db.js
@@ -27,7 +27,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect:"postgres",
-    ssl: process.env.ENVIROMENT === 'production'
+    ssl: process.env.ENVIRONMENT === 'production'
 })
 
 module.exports = sequelize;
