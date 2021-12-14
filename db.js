fb874d40eea4f2dd2ed96b862b207d8a60e9a20d
reagankporter <reagankporter@gmail.com>
2021-12-14 15:30:34 -0500

    db change

--- db.js
+++ db.js
@@ -1,19 +1,33 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect:"postgres",
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
-});
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     dialect:"postgres",
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false
+//         }
+//     }
+// });
 
-module.exports = sequelize;
+// module.exports = sequelize;
+
+
 
 // const Sequelize = require('sequelize');
 //      
 // const sequelize = new Sequelize("postgres://postgres:maple2323@localhost:5432/selfq");
 //      
 // module.exports = sequelize;
+
+
+
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:"postgres",
+    ssl: process.env.ENVIROMENT === 'production'
+});
+
+module.exports = sequelize;
