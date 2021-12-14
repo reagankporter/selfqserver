7b233fd76dd3b2a754913455aa1e626c1476b4b6
reagankporter <reagankporter@gmail.com>
2021-12-14 15:11:03 -0500

    heroku deploy

--- db.js
+++ db.js
@@ -1,19 +1,19 @@
-// const Sequelize = require('sequelize');
-
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//     dialect:"postgres",
-//     dialectOptions: {
-//         ssl: {
-//             require: true,
-//             rejectUnauthorized: false
-//         }
-//     }
-// });
-
-// module.exports = sequelize;
-
 const Sequelize = require('sequelize');
-     
-const sequelize = new Sequelize("postgres://postgres:maple2323@localhost:5432/selfq");
-     
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:"postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+});
+
 module.exports = sequelize;
+
+// const Sequelize = require('sequelize');
+//      
+// const sequelize = new Sequelize("postgres://postgres:maple2323@localhost:5432/selfq");
+//      
+// module.exports = sequelize;
